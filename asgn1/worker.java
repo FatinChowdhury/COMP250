81cfcca294de36a548d3bb1d6139d06a6a9697ed
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-09 16:44:47 -0400

    Create worker.java

---
+++ asgn1/worker.java
@@ -0,0 +1,7 @@
+class Worker extends Unit {	// might have to do extend Unit at Worker.java
+		private int jobsPerformed;
+		public Worker(Tile, double hp, String faction){
+			int movingRange = 2;
+			int jobsPerformed = 0;
+		}
+	}
