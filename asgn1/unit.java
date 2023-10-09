7bf270a1d05825cde6aa5ec4d5a4b1b70102d21a
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-09 16:41:19 -0400

    Update unit.java

--- asgn1/unit.java
+++ asgn1/unit.java
@@ -3,11 +3,11 @@ package Assignment1;
 abstract class Unit {
 	private Tile;
 	private double hp; // health points
-	private int maxReach; // how far this unit can move in a single turn
+	private int movingRange; // how far this unit can move in a single turn
 	private String faction;
 	
 	
-	public Unit(Tile, double hp, int maxReach, String faction) {
+	public Unit(Tile, double hp, int movingRange, String faction) {
 		
 	}
 	
@@ -23,12 +23,51 @@ abstract class Unit {
 		
 	}
 	
-	class Settler{
+	class Settler{	// might have to do extend Unit at Settler.java
 		
 		public Settler(Tile, double hp, String faction) {
+			int movingRange = 2;
+		}	
+	}
+	
+	class Worker {	// might have to do extend Unit at Worker.java
+		private int jobsPerformed;
+		public Worker(Tile, double hp, String faction){
+			int movingRange = 2;
+			int jobsPerformed = 0;
+		}
+	}
+	
+	abstract class MilitaryUnit { // might have to do extend Unit at MilitaryUnit.java
+		private double attackDamage;
+		private int attackRange;
+		private int armor;
+		
+		public MilitaryUnit(Tile, double hp, int movingRange, String faction, 
+				double attackDamage, int attackRange, int armor) {
+			class Warrior {
+				public Warrior(Tile, double hp, String faction) {
+					int movingRange = 1;
+					double attackDamage = 20.0;
+					int attackRange = 1;
+					int armor = 25;
+				}
+			}
+			class Archer{
+				private int arrowsAvailable;
+				
+				public Archer(Tile, double hp, String faction) {
+					int movingRange = 2;
+					double attackDamage = 15.0;
+					int attackRange = 2;
+					int armor = 0;
+					int arrowsAvailable = 5;
+				}
+			}
+			
+			
 			
 		}
-		
 	}
 	
 }
