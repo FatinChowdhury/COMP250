e11a51d6f6b59ce58e9046873ca940940f4baf28
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-09 15:38:26 -0400

    Create unit.java

---
+++ asgn1/unit.java
@@ -0,0 +1,34 @@
+package Assignment1;
+
+abstract class Unit {
+	private Tile;
+	private double hp; // health points
+	private int maxReach; // how far this unit can move in a single turn
+	private String faction;
+	
+	
+	public Unit(Tile, double hp, int maxReach, String faction) {
+		
+	}
+	
+	public final void getPosition() { // to modify void later?
+		
+	}
+	
+	public final void getHP() { // to modify void later?
+	
+	}
+	
+	public final void getFaction() { // to modify void later?
+		
+	}
+	
+	class Settler{
+		
+		public Settler(Tile, double hp, String faction) {
+			
+		}
+		
+	}
+	
+}
