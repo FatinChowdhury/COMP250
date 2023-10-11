d5c562b113295d71feb50b5530ded57e8547cea7
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-11 03:09:38 -0400

    Create listofunits.java

---
+++ asgn1/listofunits.java
@@ -0,0 +1,43 @@
+package Assignment1;
+
+public class ListOfUnits {
+	private String[] Units; // gotta fix this later
+	private int ListOfUnits = Units.length;
+	
+	public ListOfUnits() {
+		String[] Units = new String[0];	
+	}
+	public int getSize() {
+		// incomplete
+		return ListOfUnits;
+	}
+	
+	public String[] getList() {
+		// incomplete
+		return Units;
+	}
+	
+	public int getUnit(int numberUnits) {
+		// incomplete
+		return 0;
+	}
+	
+	public void addUnit(int Unit) {
+		// incomplete
+	}
+	
+	public int indexOf() {
+		// incomplete
+		return -1;
+	}
+	
+	public boolean removeUnit() {
+		// incomplete
+		return true;
+	}
+	
+	public getArmy() {
+		// incomplete
+		return;
+	}	
+}
