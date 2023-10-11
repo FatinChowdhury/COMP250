e3627708e7b41f3c5ab08372ec8306802b9e1ef4
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-11 17:23:53 -0400

    Update settler.java

--- asgn1/settler.java
+++ asgn1/settler.java
@@ -1,6 +1,6 @@
 package assignment1
 class Settler extends Unit{	// might have to do extend Unit at Settler.java	
 	public Settler(Tile, double hp, String faction) {
-	int movingRange = 2;
+		int movingRange = 2;
 	}	
 }
