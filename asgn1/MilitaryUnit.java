0e1a7476698f8742d4cfd6a07b6c401501dfbe0a
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-14 20:18:30 -0400

    Update MilitaryUnit.java

--- asgn1/MilitaryUnit.java
+++ asgn1/MilitaryUnit.java
@@ -1,6 +1,15 @@
 package Assignment1;
 
-public class MilitaryUnit {
-
+abstract class MilitaryUnit extends Unit {
+    private double attackDamage;
+    private int attackRange;
+    private int armor;
+    
+    public MilitaryUnit(Tile position, double hp, int movingRange, String faction,
+            double attackDamage, int attackRange, int armor) {
+    	super(position, hp, movingRange, faction);
+    	this.attackDamage = attackDamage;
+    	this.attackRange = attackRange;
+    	this.armor = armor;
+    }
 }
-
