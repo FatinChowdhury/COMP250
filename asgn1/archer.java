a9c2806f6ce8abc85cc3af7578d7ffaae53378cd
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-14 20:20:11 -0400

    Update archer.java

--- asgn1/archer.java
+++ asgn1/archer.java
@@ -1,1 +1,14 @@
+package Assignment1;
 
+class Archer extends MilitaryUnit {
+    private int arrowsAvailable;
+
+    public Archer(Tile position, double hp, String faction) {
+        super(position, hp, 2, faction, 15.0, 2, 0);
+        this.arrowsAvailable = 5;
+    }
+
+    private int getArrowsAvailable() {
+        return arrowsAvailable;
+    }
+}
