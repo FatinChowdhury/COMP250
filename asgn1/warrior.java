0c11688f752fdd4cacae2a310a57e9b99630b401
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-14 20:19:29 -0400

    Update warrior.java

--- asgn1/warrior.java
+++ asgn1/warrior.java
@@ -1,6 +1,8 @@
 package Assignment1;
 
-public class Warrior {
-
+class Warrior extends MilitaryUnit {
+	
+    public Warrior(Tile position, double hp, String faction) {
+        super(position, hp, 1, faction, 20.0, 1, 25);
+    }
 }
-
