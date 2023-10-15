997c4a52166613d65e3611c22f58e0efb2b69bc6
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-14 20:17:48 -0400

    Update unit.java

--- asgn1/unit.java
+++ asgn1/unit.java
@@ -1,73 +1,33 @@
 package Assignment1;
 
 abstract class Unit {
-	private Tile;
+	private Tile position;
 	private double hp; // health points
 	private int movingRange; // how far this unit can move in a single turn
 	private String faction;
 	
-	
-	public Unit(Tile, double hp, int movingRange, String faction) {
-		
+	public Unit(Tile position, double hp, int movingRange, String faction) {
+		this.position = position;
+        this.hp = hp;
+        this.movingRange = movingRange;
+        this.faction = faction;
 	}
 	
-	public final getPosition() { // to modify void later?
-		return Tile;
+	
+	public final Tile getPosition() {
+		return position;
 	}
 	
-	public final double getHP() { // to modify void later?
+	public final double getHP() {
 		return hp;
 	}
 	
-	public final String getFaction() { // to modify void later?
+	public final String getFaction() {
 		return faction;
 	}
 	
-	class Settler{	// might have to do extend Unit at Settler.java
-		
-		public Settler(Tile, double hp, String faction) {
-			int movingRange = 2;
-		}	
-	}
-	
-	class Worker {	// might have to do extend Unit at Worker.java
-		private int jobsPerformed;
-		public Worker(Tile, double hp, String faction){
-			int movingRange = 2;
-			int jobsPerformed = 0;
-		}
-	}
-	
-	abstract class MilitaryUnit { // might have to do extend Unit at MilitaryUnit.java
-		private double attackDamage;
-		private int attackRange;
-		private int armor;
-		
-		public MilitaryUnit(Tile, double hp, int movingRange, String faction, 
-				double attackDamage, int attackRange, int armor) {
-			class Warrior {
-				public Warrior(Tile, double hp, String faction) {
-					int movingRange = 1;
-					double attackDamage = 20.0;
-					int attackRange = 1;
-					int armor = 25;
-				}
-			}
-			class Archer{
-				private int arrowsAvailable;
-				
-				public Archer(Tile, double hp, String faction) {
-					int movingRange = 2;
-					double attackDamage = 15.0;
-					int attackRange = 2;
-					int armor = 0;
-					int arrowsAvailable = 5;
-				}
-			}
-			
-			
-			
-		}
-	}
-	
 }
+	
+
+			
+	
