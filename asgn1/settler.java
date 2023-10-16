ced03751c2c869447d294ce4adfafb5f769b9609
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-15 23:56:27 -0400

    Update settler.java

--- asgn1/settler.java
+++ asgn1/settler.java
@@ -1,7 +1,24 @@
-package Assignment1;
+package assignment1;
 
 class Settler extends Unit {
     public Settler(Tile position, double hp, String faction) {
         super(position, hp, 2, faction);
     }
+
+	public void takeAction(Tile targetTile) {
+		if (this.getPosition() == targetTile && !targetTile.isCity()) {
+			targetTile.buildCity();
+			removeUnit();
+		}
+	}
+	
+	public boolean equals(Object obj) {
+	    if (this == obj) {
+	    	return true;
+	    }
+		if (!super.equals(obj)) {  
+	        return false;
+		}
+	    return true;
+	}
 }
